8b1d720da373e5c1d8cc1d0e03323e3b371f125f
Derrnux <daghzep@gmail.com>
2022-02-19 16:03:08 +0100

    first dice and button visible on client

--- website/scripts.js
+++ website/scripts.js
@@ -1,1 +1,20 @@
 //for future use
+async function getDice() {
+
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: "{}"
+    }
+
+    const response = await fetch('/getDice', options)
+    const dice = await response.json();
+
+    document.getElementById("firstDice").innerHTML = dice[0];
+    document.getElementById("secondDice").innerHTML = dice[1];
+    document.getElementById("thirdDice").innerHTML = dice[2];
+    document.getElementById("fourthDice").innerHTML = dice[3];
+    document.getElementById("fifthDice").innerHTML = dice[4];
+
+    console.log(dice);
+}
